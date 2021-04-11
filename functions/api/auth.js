8263b9f0d6d63ff26091cb09c2eac4695003542d
Ato <jonathan.markin@unionsg.com>
2021-04-11 07:16:08 +0000

    Auth api done

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const randomToken = require("random-token");
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 const router = express.Router();
+const firebase = require("firebase");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDTsWh-ATsI14WVBvg_dHisp59_BsUcNy0",
+    authDomain: "iattend-e19c5.firebaseapp.com",
+    databaseURL: "https://iattend-e19c5-default-rtdb.firebaseio.com",
+    projectId: "iattend-e19c5",
+    storageBucket: "iattend-e19c5.appspot.com",
+    messagingSenderId: "695692245695",
+    appId: "1:695692245695:web:246ea36c29f784c8cf240a",
+    measurementId: "G-9S6T9D2GXX",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+// // Fetch the service account key JSON file contents
+// var serviceAccount = require("../iattendservice.json");
+
+// // Initialize the app with a service account, granting admin privileges
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://iattend-e19c5-default-rtdb.firebaseio.com/",
+// });
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
 
 router.post("/login", async(req, res, next) => {
     var data = req.body;
     var email = data.email;
     var password = data.password;
 
-    var responseCode;
-    var message;
-    var dataObject;
-    var statusCode = 200;
+    let responseCode = " ";
+    let message = " ";
+    let dataObject = null;
+    //var statusCode = 200;
 
     console.log("Email" + email + "Password" + password);
     functions.logger.log("Email", email);
@@ -26,24 +52,112 @@ router.post("/login", async(req, res, next) => {
             // Signed in
             var user = userCredential.user;
 
+            this.responseCode = "00";
+            this.message = "Login successful";
+            this.dataObject = user;
+            //statusCode = 200;
+
+            functions.logger.log("User", user);
+            functions.logger.log("Email", responseCode + message + dataObject);
+            return user;
+        })
+        .catch((error) => {
+            this.responseCode = "99";
+            this.message = error.message;
+            this.dataObject = error;
+            //statusCode = 500;
+
+            functions.logger.log("No user", error);
+            functions.logger.log("No user", error.message);
+        });
+
+    res.status(200).json({
+        responseCode: responseCode,
+        responseMessage: message,
+        responseData: dataObject,
+    });
+});
+
+router.post("/signup", async(req, res, next) => {
+    var data = req.body;
+    var email = data.email;
+    var password = data.password;
+    var firstName = data.firstName;
+    var lastName = data.lastName;
+    var groupId = data.groupId;
+
+    var responseCode = " ";
+    var message = " ";
+    var dataObject = null;
+    //var statusCode = 200;
+
+    console.log("Email" + email + "Password" + password);
+    functions.logger.log("Email", email);
+    functions.logger.log("Password", password);
+
+    //Sign in with firebase
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+
+            //Update the display name and the photo
+
+            user
+                .updateProfile({
+                    displayName: firstName + " " + lastName,
+                    photoURL: "",
+                })
+                .then((res) => {
+                    // Update successful.
+                    return res;
+                })
+                .catch((error) => {
+                    // An error happened.
+                    console.log("An error occurred");
+                    console.log(error);
+                });
+
+            //Store the user details in the user table
+
+            var usersRef = db.ref("users");
+            usersRef.set({
+                email: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    groupId: groupId,
+                    email: email,
+                    role: "Member",
+                },
+            });
+
             responseCode = "00";
             message = "Login successful";
             dataObject = user;
-            statusCode = 200;
+            //statusCode = 200;
+
+            res.status(200).json({
+                responseCode: responseCode,
+                responseMessage: message,
+                responseData: dataObject,
+            });
+
             return user;
         })
         .catch((error) => {
             responseCode = "99";
             message = error.message;
             dataObject = error;
-            statusCode = 500;
-        });
+            //statusCode = 500;
 
-    res.status(statusCode).json({
-        responseCode: responseCode,
-        responseMessage: message,
-        responseData: dataObject,
-    });
+            res.status(500).json({
+                responseCode: responseCode,
+                responseMessage: message,
+                responseData: dataObject,
+            });
+        });
 });
 
 router.post("/logout", (req, res, next) => {
@@ -64,3 +178,5 @@ router.post("/logout", (req, res, next) => {
         message: message,
     });
 });
+
+module.exports = router;
--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./iattendservice.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://iattend-e19c5-default-rtdb.firebaseio.com/",
+});
 app.use(cors({ origin: true }));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,25 +21,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const authRoutes = require("./api/auth");
-const attendanceRoutes = require("./api/attendance");
-const eventsRoutes = require("./api/events");
-const membersRoutes = require("./api/members");
-const noticesRoutes = require("./api/notices");
-const requestsRoutes = require("./api/requests");
-const teamsRoutes = require("./api/teams");
-const userRoutes = require("./api/user");
+// const attendanceRoutes = require("./api/attendance");
+// const eventsRoutes = require("./api/events");
+// const membersRoutes = require("./api/members");
+// const noticesRoutes = require("./api/notices");
+// const requestsRoutes = require("./api/requests");
+// const teamsRoutes = require("./api/teams");
+// const userRoutes = require("./api/user");
 
 app.use("/auth", authRoutes);
-app.use("/attend", attendanceRoutes);
-app.use("/events", eventsRoutes);
-app.use("/members", membersRoutes);
-app.use("/notices", noticesRoutes);
-app.use("/requests", requestsRoutes);
-app.use("/teams", teamsRoutes);
-app.use("/user", userRoutes);
+// app.use("/attend", attendanceRoutes);
+// app.use("/events", eventsRoutes);
+// app.use("/members", membersRoutes);
+// app.use("/notices", noticesRoutes);
+// app.use("/requests", requestsRoutes);
+// app.use("/teams", teamsRoutes);
+// app.use("/user", userRoutes);
 
-app.get("/hello-world", (req, res) => {
-    return res.status(200).send("Hello World!");
+// app.get("/hello-world", (req, res) => {
+//     return res.status(200).send("Hello World!");
+// });
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
 });
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        message: error.message,
+    });
+});
+
+//module.exports = app;
 exports.app = functions.https.onRequest(app);
