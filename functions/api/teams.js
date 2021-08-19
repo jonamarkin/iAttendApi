1f2987e64c2cd115f2f58038e5474d03c8b563eb
Ato <jonathan.markin@unionsg.com>
2021-08-19 16:09:18 +0000

    Get team members endpoint

--- functions/api/teams.js
+++ functions/api/teams.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const { v1: uuidv1, v4: uuidv4 } = require("uuid");
 
 const db = admin.firestore();
+
+//Create a new team
 router.post("/create", async(req, res, next) => {
     var data = req.body;
     var name = data.email;
@@ -35,6 +37,69 @@ router.post("/create", async(req, res, next) => {
             return res.status(500).json({
                 responseCode: "777",
                 responseMessage: "Team creation failed!",
+                responseData: error,
+            });
+        });
+});
+
+//Get all Teams
+router.get("/getAll", async(req, res, next) => {
+    db.collection("teams")
+        .get()
+        .then((querySnapshot) => {
+            var teams = [];
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+
+                console.log(doc.id, " => ", doc.data());
+
+                var teamObject = doc.data();
+                teamObject.id = doc.id;
+
+                teams.push(teamObject);
+            });
+            return res.status(200).json({
+                responseCode: "000",
+                responseMessage: "Successfully retrieved teams!",
+                responseData: teams,
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                responseCode: "777",
+                responseMessage: "Failed to get teams!",
+                responseData: error,
+            });
+        });
+});
+
+router.get("/getMembers/:teamId", async(req, res, next) => {
+    var teamId = req.params.teamId;
+    db.collection("users")
+        .where("teams", "array-contains", teamId)
+        .get()
+        .then((querySnapshot) => {
+            var teamMembers = [];
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+
+                console.log(doc.id, " => ", doc.data());
+
+                var teamMember = doc.data();
+                teamMember.id = doc.id;
+
+                teamMembers.push(teamMember);
+            });
+            return res.status(200).json({
+                responseCode: "000",
+                responseMessage: "Successfully retrieved team members!",
+                responseData: teamMembers,
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                responseCode: "777",
+                responseMessage: "Failed to get team members!",
                 responseData: error,
             });
         });
